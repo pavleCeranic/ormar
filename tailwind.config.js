ed3e6d34ff28b5407cf46f4826a3dc54e84f10e5
Pavle Ceranic <pavlepetness@gmail.com>
2024-03-08 21:20:25 +0100

    [001] creating react app called myapp
    first steps

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{js,ts,jsx,tsx}', // Include JavaScript and TypeScript files in the src directory
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
