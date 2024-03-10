3119f87a73edc6d06236d236217c00b8d6fa91a1
Pavle Ceranic <pavlepetness@gmail.com>
2024-03-10 17:11:12 +0100

    [002] Added basic three parted heading and added custom colors to tailwind config

--- myapp/src/Heading.js
+++ myapp/src/Heading.js
@@ -2,12 +2,27 @@ import './Heading.css';
 
 function Heading() {
   return (
-    <div className='w-full h-10 flex justify-between'>
-        <div className='p-4'>logo</div>
-        <div className='p-4'>menu</div>
-        <div className='p-4'>account</div>
+    <div className='w-full flex justify-between'>
+      <div className='self-center text-center m-5 w-36 '>logo</div>
+      <Menu label="Collections" items={['Item 1', 'Item 2', 'Item 3']} />
+      <div className='self-center text-center m-5 w-36 hover:font-bold transition-all duration-200 ease-in-out hover:bg-gray-300'>Account</div>
     </div>
   );
 }
+
+const Menu = ({ label, items }) => {
+  return (
+    <div className="self-center relative group">
+      <span className="text-center m-5 w-36 hover:font-bold transition-all duration-200 ease-in-out hover:bg-gray-300">
+        {label}
+      </span>
+      <div className="hidden group-hover:block absolute z-10 bg-white p-2 mt-2 space-y-2 shadow">
+        {items.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Heading;
