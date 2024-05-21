1963032a96d4763bf0908acd14bc8eb64e31ca4b
pavleCeranic <pavle.ceranic@develabs.net>
2024-05-21 16:34:50 +0200

    [003] upgrading form validation
    https://app.clickup.com/t/86byvkc08
    003

--- myapp/src/Pages/LoginRegistration.js
+++ myapp/src/Pages/LoginRegistration.js
@@ -1,19 +1,24 @@
 import './LoginRegistration.css';
 import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 const LoginRegistration = () => {
 
-	const firstDivRef = useRef(null);
-	const secondDivRef = useRef(null);
-	const [activeTab, setActiveTab] = useState('login');
-	const [formData, setFormData] = useState({
+	const initialState = {
 		username: '',
 		email: '',
 		password: ''
-	});
+	}
+
+	const firstDivRef = useRef(null);
+	const secondDivRef = useRef(null);
+	const emailRef = useRef(null);
+	const usernameRef = useRef(null);
+	const [activeTab, setActiveTab] = useState('login');
+	const [loginFormData, setLoginFormData] = useState(initialState);
+	const [regFormData, setRegFormData] = useState(initialState);
 
 	const handleTabClick = (activeTab, notActiveTab) => {
 
@@ -22,10 +27,13 @@ const LoginRegistration = () => {
 		if (activeTab && notActiveTab) {
 			activeTab.current.style.backgroundColor = 'white';
 			notActiveTab.current.style.backgroundColor = '#d3d3d3';
+
+			// validateEmail(emailRef.current.value);
 		}
 	};
 
 	const handleInputChange = (e) => {
+
 		let name = e.target.id;
 		let value = e.target.value;
 
@@ -34,10 +42,17 @@ const LoginRegistration = () => {
 			return
 		}
 
-		setFormData(prevState => ({
-			...prevState,
-			[name]: value.replaceAll(' ', '')
-		}));
+		activeTab === 'login'
+			?
+			setLoginFormData(prevState => ({
+				...prevState,
+				[name]: value.replaceAll(' ', '')
+			}))
+			: setRegFormData(prevState => ({
+				...prevState,
+				[name]: value.replaceAll(' ', '')
+			}));
+
 	};
 
 	useEffect(() => {
@@ -47,15 +62,39 @@ const LoginRegistration = () => {
 		handleTabClick(firstDivRef, secondDivRef);
 	}, []);
 
+	const validateEmail = (e) => {
+
+		setTimeout(() => {
+			if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(e.target?.value)) {
+				// alert user that email is not valid
+
+				if (e.target.style.outline !== 'red solid 1px') {
+					e.target.classList.add('wiggle');
+					e.target.classList.add('myoutline');
+
+					setTimeout(() => {
+						e.target.classList.remove('wiggle');
+					}, 200);
+				}
+
+				e.target.style.outline = 'red solid 1px';
+
+				return;
+			}
+			else {
+				e.target.style.outline = 'green solid 1px';
+				setTimeout(() => {
+					// e.target.style.outline = '';
+				}, 2000);
+			}
+		}, 1000);
+
+	}
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email)) {
-			// alert user that email is not valid
-			return;
-		}
-
-		console.log('e', formData)
+		// console.log('e', formData)
 		// axios.post('___', formData)
 		// 	.then(response => {
 		// 		console.log('Form submitted successfully:', response.data);
@@ -82,16 +121,16 @@ const LoginRegistration = () => {
 				<div className='w-full h-5/6 flex flex-col justify-center items-center'>
 					{activeTab === 'login' ? (
 						<form className='flex flex-col w-full items-center' onSubmit={handleSubmit}>
-							<input type='text' id='email' placeholder='Email' onChange={handleInputChange} value={formData.email} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
-							<input type='password' id='password' placeholder='Password' onChange={handleInputChange} value={formData.password} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
+							<input type='text' id='email' ref={emailRef} placeholder='Email' onChange={handleInputChange} onBlur={validateEmail} value={loginFormData.email} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
+							<input type='password' id='password' placeholder='Password' onChange={handleInputChange} value={loginFormData.password} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
 							<Link to="/" className=''>Forgot Your Password? </Link>
 							<button type='submit' className='bg-black text m-2 p-2 w-4/5 cursor-pointer text-white text-center hover:bg-buttonYellow transition-colors'>SIGN IN</button>
 						</form>
 					) : (
 						<form className='flex flex-col w-full justify-center items-center' onSubmit={handleSubmit}>
-							<input type='text' id='email' placeholder='Email' onChange={handleInputChange} value={formData.email} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
-							<input type='text' id='username' placeholder='Username' onChange={handleInputChange} value={formData.username} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
-							<input type='password' id='password' placeholder='Password' onChange={handleInputChange} value={formData.password} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
+							<input type='text' id='email' ref={emailRef} placeholder='Email' onChange={handleInputChange} onBlur={validateEmail} value={regFormData.email} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
+							<input type='text' id='username' ref={usernameRef} placeholder='Username' onChange={handleInputChange} value={regFormData.username} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
+							<input type='password' id='password' placeholder='Password' onChange={handleInputChange} value={regFormData.password} className='outline-0 shadow-2xl m-2 p-2 w-4/5 focus:shadow-lg focus:scale-105 transition-all duration-200 ease-in-out' />
 							<Link to="/account" className='' onClick={() => { handleTabClick(firstDivRef, secondDivRef) }}>Already have an Account? </Link>
 							<button type='submit' className='bg-black text m-2 p-2 w-4/5 cursor-pointer text-white text-center hover:bg-buttonYellow transition-colors'>REGISTER</button>
 						</form>
