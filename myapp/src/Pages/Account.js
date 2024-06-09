8f2d53aa7a90ad8e547a08147c05a756d8e981ee
Pavle Ceranic <pavlepetness@gmail.com>
2024-06-09 22:48:55 +0200

    added section for displaying users articles in acconut and more minor changes

--- myapp/src/Pages/Account.js
+++ myapp/src/Pages/Account.js
@@ -1,14 +1,34 @@
 import './Account.css';
+import BestSellerContainer from '../Components/BestSellerContainer.js'
+import { useEffect, useRef, useState } from 'react';
 
 const Account = () => {
 
+	const firstDivRef = useRef(null);
+	const secondDivRef = useRef(null);
+	const [activeTab, setActiveTab] = useState('ormar');
+
+	const handleTabChange = (newActiveTab, oldActiveTab) => {
+
+		setActiveTab(newActiveTab === firstDivRef ? 'ormar' : '');
+
+		if (newActiveTab && oldActiveTab) {
+			newActiveTab.current.style.backgroundColor = 'white';
+			oldActiveTab.current.style.backgroundColor = '#d3d3d3';
+		}
+	}
+
+	useEffect(() => {
+		handleTabChange(firstDivRef, secondDivRef)
+	}, [])
+
 	return (
 		<div className='flex flex-col content-center mt-24 items-center'>
-			<div className='flex flex-row h-56 w-3/5 bg-buttonYellow justify-around items-center'>
+			<div className='flex flex-row h-56 w-3/5 justify-around items-center'>
 				<div className='flex m-6'>
 					<div className='flex flex-col w-44 h-44 rounded-full bg-zinc-400 items-center justify-end overflow-hidden' >
-						<div className=' w-14 h-14 rounded-full bg-zinc-600 mb-2'></div>
-						<div className=' w-32 h-20 rounded-3xl bg-zinc-600 '></div>
+						<div className=' w-12 h-12 rounded-full bg-zinc-600 mb-2'></div>
+						<div className=' w-28 h-16 rounded-3xl bg-zinc-600'></div>
 					</div>
 				</div>
 				<div className='flex flex-col flex-1'>
@@ -17,10 +37,60 @@ const Account = () => {
 					<div> X Banjaluka</div>
 					<div>Posljednji put vidjen u 13:44</div>
 				</div>
-				<div>actions</div>
+				<div>
+					<div></div>
+					<button>Edit account</button>
+				</div>
 			</div>
-			<div className='flex flex-row h-80 w-3/5 bg-inactiveGrey'>container for displaying articles</div>
+			<div className='flex flex-col h-full w-3/5 mb-4 shadow-2xl'>
+				<div className='flex flex-row justify-around'>
+					<div className=' flex justify-center items-center h-9 cursor-pointer w-full' ref={firstDivRef} onClick={() => { handleTabChange(firstDivRef, secondDivRef) }}>Ormar</div>
+					<div className=' flex justify-center items-center h-9 cursor-pointer w-full' ref={secondDivRef} onClick={() => { handleTabChange(secondDivRef, firstDivRef) }}>Ocjene</div>
+				</div>
+				<div className='overflow-hidden h-full mt-3'>
+					{activeTab === 'ormar' ?
+						<BestSellerContainer title='' />
+						: <Reviews />
+					}
+				</div>
+
+			</div>
 		</div>
+	)
+}
+
+const Reviews = () => {
+
+	const reviews = [
+		{ reviewer: 'Pero', review: 'Super kupac', stars: 4, date: 'Decembar 2024' },
+		{ reviewer: 'Mico', review: 'nisam zadovoljan komunikacijom', stars: 2, date: 'Decembar 1984' },
+		{ reviewer: 'Jovo', review: 'sve pet', stars: 5, date: 'Januar 2024' },
+	]
+
+	return (
+		<div className='flex flex-col h-96'>
+			{reviews.length > 0
+				? reviews.map((item, index) => {
+					<div className='flex flex-col'>
+						<div key={index} className='flex flex-row w-full h-20'>
+							<div>
+								<div className='w-10 h-10 bg-black m-3'></div>
+							</div>
+							<div>
+								<div>{item.reviewer}</div>
+								<div>{item.stars}</div>
+							</div>
+							<div>
+								<div>{item.date}</div>
+							</div>
+						</div>
+						<div className='w-full h-7'>{item.review}</div>
+					</div>
+
+				})
+				: <div className='flex justify-center'>Trenutno nema ocjena</div>}
+		</div>
+
 	)
 }
 
--- myapp/src/Pages/Home.js
+++ myapp/src/Pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
 	return (
 		<div className=''>
 			<CarouselContainer />
-			<BestSellerContainer />
+			<BestSellerContainer  title='Best Seller'/>
 		</div>
 	);
 }
